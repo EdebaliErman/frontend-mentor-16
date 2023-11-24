d190b8c269b0990e24a2216f092313857a930291
EdebaliErman <edebalierman@gmail.com>
2023-11-24 18:56:16 +0300

    Assets oluşturuldu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './Components/About/About';
+import Footer from './Components/Footer/Footer';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-     
+      <Home />
+      <About />
+      <Footer />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'baiJamjuree': ['Bai Jamjuree', 'sans-serif']
+      },
+      colors: {
+        'strongCyan': "hsl(171, 66%, 44%)",
+        "lightBlue": "hsl(233, 100%, 69%)",
+        "darkGrayishBlue": "hsl(210, 10%, 33%)",
+        "Grayish Blue": "hsl(201, 11%, 66%)"
+      }
+    },
   },
   plugins: [],
 }
